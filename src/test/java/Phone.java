7aca5447ba7a0f309339f7455c7b9c5fd6c76813
Dmytro Iukhymenko <diukh@softserveinc.com>
2018-07-31 13:28:58 +0300

    print method added to Phone class; call method changed; IF fixed.

--- src/test/java/Phone.java
+++ src/test/java/Phone.java
@@ -1,20 +1,24 @@
 public class Phone {
-    String number;
+    String phoneType;
 
-    public Phone(String number) {
-        this.number = number;
+    public Phone(String phoneType) {
+        this.phoneType = phoneType;
     }
 
-    public void call() {
+    public void call(String number) {
         System.out.println("Dialing - " + number);
     }
 
     public boolean batteryStatus(boolean res) {
-        if (res == true) {
+        if (res) {
             System.out.println("Battery is full");
         } else {
             System.out.println("Low battery");
         }
         return res;
+    }
+
+    public void print(){
+        System.out.println("You have chosen " + phoneType + " phone");
     }
 }
