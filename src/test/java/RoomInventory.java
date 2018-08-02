2e898b5cfe2c8d34e05d6d693db2fabf122f9e43
Dmytro Iukhymenko <yukhymenkods@gmail.com>
2018-08-03 00:04:25 +0300

    Interface implementation

--- src/test/java/RoomInventory.java
+++ src/test/java/RoomInventory.java
@@ -12,10 +12,8 @@ public class RoomInventory {
         Table roomTable = new Table("green", 15);
         Chair roomChair = new Chair("red", 3, "bubble");
         Chair roomChair2 = new Chair("yellow", 13, "rectangle");
-//        Phone roomPhone = new Phone("mobile");
-//        roomPhone.batteryStatus(false);
-//        roomPhone.call("380990288777");
-//        roomPhone.call("380674737213");
+        Phone roomPhone = new Phone("mobile");
+        roomPhone.call("380674737213");
         PrintStream myFile = new PrintStream(new FileOutputStream("we.txt", false));
         RoomInventory rI = new RoomInventory();
         rI.prCr(myFile, roomTable, roomChair, roomChair2);
