d1672503b371f5a5362b3e8f8ab544fa7baa1130
Dmytro Iukhymenko <yukhymenkods@gmail.com>
2018-08-03 00:01:50 +0300

    Interface implementation

--- src/test/java/Phone.java
+++ src/test/java/Phone.java
@@ -1,23 +1,32 @@
 import java.io.PrintStream;
+import java.util.concurrent.TimeUnit;
 
 public class Phone implements Creature{
     String phoneType;
+    long battery = TimeUnit.HOURS.toMillis(24);
+    int callsCount;
+    long callReduce = TimeUnit.SECONDS.toMillis(100);
+    long curTime = System.currentTimeMillis();
+    long batteryPercentage = ((battery - curTime - (callsCount * callReduce)));
 
-    public Phone(String phoneType) {
+
+    public Phone(String phoneType, int callsCount) {
         this.phoneType = phoneType;
+        this.callsCount = callsCount;
     }
 
     public void call(String number) {
         System.out.println("Dialing - " + number);
     }
 
-    public boolean batteryStatus(boolean res) {
-        if (res) {
-            System.out.println("Battery is full");
+    public long batteryStatus() {
+        if (batteryPercentage > 0) {
+            System.out.println("battery is " + batteryPercentage + "%");
+            //callsCount++;
         } else {
-            System.out.println("Low battery");
+            System.out.println("Bye Bye!");
         }
-        return res;
+        return batteryPercentage;
     }
 
     public void print(PrintStream out){
