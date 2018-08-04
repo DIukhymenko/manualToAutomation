bb13ead6184a29a9bf41f205f3f39e5df1306fdd
Dmytro Iukhymenko <yukhymenkods@gmail.com>
2018-08-04 15:11:11 +0300

    Save room's and attendee's inventory into the file

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -8,13 +8,14 @@ public class App {
         Chair roomChair = new Chair("red", 3, "bubble");
         Chair roomChair2 = new Chair("yellow", 13, "rectangle");
         Phone roomPhone = new Phone("mobile");
-        Thread.sleep(10000);
-        roomPhone.call("380990288777");
-        roomPhone.call("380674737213");
-        roomPhone.call("123123123");
-        System.out.println(roomPhone.batteryPercentage());
+        AttendeesInventory a1 = new AttendeesInventory("macbook", 3, true);
+//        Thread.sleep(10000);
+//        roomPhone.call("380990288777");
+//        roomPhone.call("380674737213");
+//        roomPhone.call("123123123");
+//        System.out.println(roomPhone.batteryPercentage());
         PrintStream myFile = new PrintStream(new FileOutputStream("we.txt", false));
         Room rI = new Room();
-        rI.prCr(myFile, roomTable, roomChair, roomChair2);
+        rI.prCr(myFile, roomTable, roomChair, roomChair2, a1);
     }
 }
