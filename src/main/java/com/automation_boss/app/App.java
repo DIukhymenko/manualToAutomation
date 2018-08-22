79dbb2ffed4a01afb8de00657762776e8de65697
Dmytro Iukhymenko <yukhymenkods@gmail.com>
2018-08-22 17:17:48 +0300

    Add equals and hashCode methods to the Laptop class

--- src/main/java/com/automation_boss/app/App.java
+++ src/main/java/com/automation_boss/app/App.java
@@ -3,6 +3,7 @@ package com.automation_boss.app;
 import com.automation_boss.area.Room;
 import com.automation_boss.attendies.Attendee;
 import com.automation_boss.inventory.*;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -14,8 +15,8 @@ public class App {
         Chair roomChair = new Chair("red", 3, "bubble");
         Chair roomChair2 = new Chair("yellow", 13, "rectangle");
         Chair roomChair3 = new Chair("yellow", 13, "rectangle");
-        Laptop laptop1 = new Laptop("LAPTOP1");
-        Laptop laptop2 = new Laptop("LAPTOP2");
+        Laptop laptop1 = new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true);
+        Laptop laptop2 = new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true);
         CellPhone mobile1 = new CellPhone("MOB1");
         CellPhone mobile2 = new CellPhone("MOB2");
         NoteBook firstNotebook = new NoteBook("Tetradka", 60, "yellow", "paper");
@@ -23,13 +24,13 @@ public class App {
         PaperSheets firstSheetsSet = new PaperSheets(5, 50);
         PaperSheets secondSheetsSet = new PaperSheets(5, 50);
         DeskPhone roomDeskPhone = new DeskPhone("mobile");
-//        Thread.sleep(10000);
-//        roomDeskPhone.call("380990288777");
-//        roomDeskPhone.call("380674737213");
-//        roomDeskPhone.call("123123123");
-//        mobile1.call("8888888888");
-//        mobile1.call("777777");
-//        mobile1.call("6666666666");
+        //        Thread.sleep(10000);
+        //        roomDeskPhone.call("380990288777");
+        //        roomDeskPhone.call("380674737213");
+        //        roomDeskPhone.call("123123123");
+        //        mobile1.call("8888888888");
+        //        mobile1.call("777777");
+        //        mobile1.call("6666666666");
         try {
             roomDeskPhone.contactsList();
         } catch (IOException e) {
@@ -62,6 +63,11 @@ public class App {
             System.out.println("true - " + firstNotebook.toString() + " - " + secondNotebook.toString());
         else {
             System.out.println("false - " + firstNotebook.toString() + " - " + secondNotebook.toString());
+        }
+        if (laptop1.equals(laptop2))
+            System.out.println("true - " + laptop1.toString() + " - " + laptop2.toString());
+        else {
+            System.out.println("false - " + laptop1.toString() + " - " + laptop2.toString());
         }
     }
 }
--- src/main/java/com/automation_boss/inventory/Laptop.java
+++ src/main/java/com/automation_boss/inventory/Laptop.java
@@ -1,15 +1,54 @@
 package com.automation_boss.inventory;
 
 import java.io.PrintStream;
+import java.util.Objects;
 
 public class Laptop implements Inventory {
-    protected String laptop;
+    protected String laptopName;
+    protected int width;
+    protected boolean isRetina;
+    protected int batterySize;
+    protected String color;
+    protected int monitorSize;
+    protected int ram;
+    protected boolean isSSD;
 
-    public Laptop(String laptop) {
-        this.laptop = laptop;
+    public Laptop(String laptopName, int width, boolean isRetina, int batterySize, String color, int monitorSize,
+            int ram, boolean isSSD) {
+        this.laptopName = laptopName;
+        this.width = width;
+        this.isRetina = isRetina;
+        this.batterySize = batterySize;
+        this.color = color;
+        this.monitorSize = monitorSize;
+        this.ram = ram;
+        this.isSSD = isSSD;
     }
 
     public void print(PrintStream out) {
-        out.println("Laptop - " + laptop);
+        out.println("Laptop - " + laptopName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Laptop laptop = (Laptop) o;
+        return isRetina == laptop.isRetina && batterySize == laptop.batterySize && monitorSize == laptop.monitorSize
+                && ram == laptop.ram && isSSD == laptop.isSSD;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(isRetina, batterySize, monitorSize, ram, isSSD);
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" + "isRetina=" + isRetina + ", batterySize=" + batterySize + ", monitorSize=" + monitorSize
+                + ", ram=" + ram + ", isSSD=" + isSSD + '}';
     }
 }
