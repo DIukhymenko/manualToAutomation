f6dd56fb08bec5bd14171dce895fa65543a7bfe0
Dmytro Iukhymenko <yukhymenkods@gmail.com>
2018-09-05 13:28:17 +0300

    Clean code

--- src/main/java/com/automation_boss/app/App.java
+++ src/main/java/com/automation_boss/app/App.java
@@ -10,46 +10,48 @@ import java.io.PrintStream;
 
 public class App {
     public static void main(String args[]) {
-        Chair roomChair = new Chair("red", 3, "bubble");
-        Chair roomChair2 = new Chair("yellow", 13, "rectangle");
-        Laptop laptop1 = new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true);
-        Laptop laptop2 = new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true);
-        CellPhone mobile1 = new CellPhone("MOB1", true, 100, false);
-        CellPhone mobile2 = new CellPhone("MOB1", false, 100, false);
-        NoteBook firstNotebook = new NoteBook("Tetradka", 60, "yellow", "paper");
-        NoteBook secondNotebook = new NoteBook("KrutaiaTetradka", 60, "yellow", "paper");
-        PaperSheets firstSheetsSet = new PaperSheets(5, 50);
         DeskPhone roomDeskPhone = new DeskPhone("mobile", 23, "blac1k");
         DeskPhone roomDeskPhone2 = new DeskPhone("mobile", 23, "black");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            System.out.println("Impossible to wait for 10 seconds");
-        }
         roomDeskPhone.call("380990288777");
         roomDeskPhone.call("380674737213");
         roomDeskPhone.call("123123123");
-        App cM = new App();
-        cM.inventoryComparing(roomDeskPhone, roomDeskPhone2);
-        cM.inventoryComparing(firstNotebook, secondNotebook);
-        cM.inventoryComparing(mobile1, mobile2);
-        cM.inventoryComparing(mobile1, roomChair);
         try {
             roomDeskPhone.contactsList();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("Impossible to wait for 10 seconds");
+        }
+        CellPhone mobile1 = new CellPhone("MOB1", true, 100, false);
+        CellPhone mobile2 = new CellPhone("MOB1", false, 100, false);
+        Chair roomChair = new Chair("red", 3, "bubble");
+        Chair roomChair2 = new Chair("yellow", 13, "rectangle");
+        NoteBook firstNotebook = new NoteBook("Tetradka", 60, "yellow", "paper");
+        NoteBook secondNotebook = new NoteBook("KrutaiaTetradka", 60, "yellow", "paper");
+        App cM = new App();
+        cM.inventoryComparing(roomDeskPhone, roomDeskPhone2);
+        cM.inventoryComparing(firstNotebook, secondNotebook);
+        cM.inventoryComparing(mobile1, mobile2);
+        cM.inventoryComparing(mobile1, roomChair);
         PrintStream myFile;
         try {
             myFile = new PrintStream(new FileOutputStream("test.txt", false));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        Attendee firstAttendee = new Attendee(mobile1, laptop1, firstNotebook);
-        Attendee secondAttendee = new Attendee(laptop2, mobile2, firstSheetsSet);
-
+        Attendee firstAttendee = new Attendee(
+                mobile1,
+                new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true),
+                firstNotebook
+        );
+        Attendee secondAttendee = new Attendee(
+                mobile2,
+                new Laptop("LAPTOP1", 12, true, 200, "yellow", 13, 2, true),
+                new PaperSheets(5, 50)
+        );
         Room r1 = new Room(
                 new Table("green", 15),
                 new Chair[] { roomChair, roomChair2 },
@@ -60,10 +62,6 @@ public class App {
         r1.describeInventory(myFile);
         mobile1.call("8888888888");
         mobile1.call("777777");
-        mobile1.call("6666666666");
-        mobile1.call("555555555");
-        mobile1.call("44444444");
-        mobile1.call("3333333333");
     }
 
     public void inventoryComparing(Inventory firstElement, Inventory secondElement) {
